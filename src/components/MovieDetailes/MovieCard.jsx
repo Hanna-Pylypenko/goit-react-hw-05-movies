66ea6ae95ff044aafd9a902e7d133ab9804e71dc
Hanna-Pylypenko <pilipenkog679@gmail.com>
2022-08-12 09:56:52 +0300

    added some styles

--- src/components/MovieDetailes/MovieCard.jsx
+++ src/components/MovieDetailes/MovieCard.jsx
@@ -1,26 +1,29 @@
 import { moviesApi } from 'services/moviesApi';
+import css from './MovieCard.module.css';
 export const MovieCard = ({ movieData }) => {
   const { title, name, poster_path, overview, genres, vote_average } =
     movieData;
   console.log(genres);
   return (
-    <div>
+    <div className={css.movieCard}>
       <img
         src={`${moviesApi.POSTER_URL}${poster_path}`}
         width="250px"
         alt={title || name}
       />
-      <h2 className="movieTitle"> {title || name}</h2>
-      <p>User Score: {Math.round(vote_average * 10)}%</p>
-      <p>Overview: {overview}</p>
-      <p>
-        Genres:{' '}
-        {genres
-          .map(genre => {
-            return genre.name;
-          })
-          .join(', ')}
-      </p>
+      <div className={css.movieInfo}>
+        <h2 className={css.movieTitle}> {title || name}</h2>
+        <p>User Score: {Math.round(vote_average * 10)}%</p>
+        <p>Overview: {overview}</p>
+        <p>
+          Genres:{' '}
+          {genres
+            .map(genre => {
+              return genre.name;
+            })
+            .join(', ')}
+        </p>
+      </div>
     </div>
   );
 };
